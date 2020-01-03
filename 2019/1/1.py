82b4bfeea81b4c02c4b003c6fb183e2a4ab86707
Jon Ball <jonathon.ball@gmail.com>
2020-01-03 12:29:12 -0500

    I like it better like this

--- 2019/1/1.py
+++ 2019/1/1.py
@@ -1,10 +1,15 @@
 import sys
 import math
 
+
+def read_mass_file():
+    return [int(line.strip()) for line in sys.stdin]
+
+
 def calculate_fuel(mass):
     return math.floor(mass / 3) - 2
 
 total = 0
-for mass in [int(line.strip()) for line in sys.stdin]:
+for mass in read_mass_file():
     total += calculate_fuel(mass)
 print(total)
