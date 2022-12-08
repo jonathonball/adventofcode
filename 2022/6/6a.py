6c274375c8502396c64159b4f50b86433bc05364
Jon Ball <jonathon.ball@gmail.com>
2022-12-08 07:45:59 -0500

    Completed day 6, part 2

---
+++ 2022/6/6a.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+import argparse
+import pathlib
+import sys
+from collections import deque
+
+parser = argparse.ArgumentParser()
+parser.add_argument("file", type=pathlib.Path, help="Input file")
+args = parser.parse_args()
+
+if not args.file.is_file():
+  print(args.file, end='')
+  print(" is not a file.")
+  sys.exit(255)
+
+try:
+  file = open(args.file)
+except Exception as err:
+  print(str(err))
+  print("Could not open that file. :(")
+  sys.exit(255)
+
+bufferSize = 14
+rawInput = deque(file.readline().strip())
+buffer = deque()
+for i in range(bufferSize):
+  buffer.append(rawInput.popleft())
+index = 14
+for char in rawInput:
+  if len(set(buffer)) == bufferSize:
+    print(index)
+    sys.exit()
+  else:
+    buffer.popleft()
+    buffer.append(char)
+    index += 1
+print(index)
