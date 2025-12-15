fced1a4acc47e5bc508c3684c2065d7e3164e843
Jon Ball <jonathon.ball@gmail.com>
2025-12-15 13:33:43 -0500

    fix bug

--- 2025/1/classes/__init__.py
+++ 2025/1/classes/__init__.py
@@ -1,4 +1,3 @@
 from .debugable import Debugable
 from .rotating_number import RotatingNumber
 from .input_reader import InputReader
-from .challenge import Challenge
--- 2025/1/classes/rotating_number.py
+++ 2025/1/classes/rotating_number.py
@@ -1,15 +1,19 @@
-class RotatingNumber:
-    def __init__(self):
+from .debugable import Debugable
+
+class RotatingNumber(Debugable):
+    def __init__(self, debug=False):
+        self.debug_mode = debug
         self.value = 50
         self.minimum = 0
         self.maximum = 99
         self.zero_count = 0
-        self.zero_pass = 0
 
     def rotate(self, direction, amount):
+        self.debug(f"Rotating {direction} by {amount}.")
         if direction == 'L':
             self.value = (self.value - amount) % (self.maximum + 1)
         else:
             self.value = (self.value + amount) % (self.maximum + 1)
+        self.debug(f"New value: {self.value}")
         if self.value == 0:
             self.zero_count += 1
